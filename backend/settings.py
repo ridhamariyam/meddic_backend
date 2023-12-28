7d586d5e6208ea7f145be5923fd0b6a9170ca3cb
ridha <ridhamariyam@gmail.com>
2023-12-29 00:45:58 +0530

    Cors issue

--- backend/settings.py
+++ backend/settings.py
@@ -43,10 +43,8 @@ DEBUG = config('DEBUG', cast=bool, default=True)
 
 AUTH_USER_MODEL = 'medcoapp.account'
 
-CORS_ALLOWED_ORIGINS = [
-  "https://medco-frontend.vercel.app/",
-   
-]
+
+CORS_ALLOW_HEADERS = "access-control-allow-origin"
 
 
 
