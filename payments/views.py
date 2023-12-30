d7f3fae5e8c7e53e71e15e80bd74b65f10c5f72b
ridha <ridhamariyam@gmail.com>
2023-12-30 09:43:36 +0530

    payment 404 error debugging

--- payments/views.py
+++ payments/views.py
@@ -37,11 +37,11 @@ def test_payment(request):
                 },
             ],
             mode='payment',
-            # success_url=f"http://localhost:3000/BookDoctor/{id}?is_success=true&&mode={mode}&&date={date}&&time={time}&&doctor={doctor}",
-            # cancel_url='http://localhost:3000/BookDoctor/')
+            success_url=f"http://localhost:3000/BookDoctor/{id}?is_success=true&&mode={mode}&&date={date}&&time={time}&&doctor={doctor}",
+            cancel_url='http://localhost:3000/BookDoctor/')
             
-            success_url=f"https://medco-frontend.vercel.app/BookDoctor/{id}?is_success=true&&mode={mode}&&date={date}&&time={time}&&doctor={doctor}",
-            cancel_url='https://medco-frontend.vercel.app/BookDoctor/')
+            # success_url=f"https://medco-frontend.vercel.app/BookDoctor/{id}?is_success=true&&mode={mode}&&date={date}&&time={time}&&doctor={doctor}",
+            # cancel_url='https://medco-frontend.vercel.app/BookDoctor/')
     
     return Response(status=status.HTTP_200_OK,data={'url':session.url})
 
